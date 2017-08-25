73a00ade4b32087347839a5fd1b42d04a98989ff
Gary Kovar <gkovar@gmail.com>
2017-08-25 12:13:36 -0400

    adds input to admin bar

--- includes/class-admin-search-input.php
+++ includes/class-admin-search-input.php
@@ -39,6 +39,21 @@ class VS_Admin_Search_Input {
 	 * @since  0.1.0
 	 */
 	public function hooks() {
+		add_action( 'admin_bar_menu', array( $this, 'add_input' ), PHP_INT_MAX );
+	}
 
+	public function add_input( $wp_admin_bar ) {
+		$args = array(
+			'id'    => 'vocal_search_input',
+			'title'  => $this->get_input_markup(),
+		);
+		$wp_admin_bar->add_node( $args );
+	}
+
+	private function get_input_markup() {
+		return '<form id="vocal-search" action="admin.php">
+					<input name="s">
+					<input name="page" value="vocal_search_admin_search" type="hidden">
+				</form>';
 	}
 }
