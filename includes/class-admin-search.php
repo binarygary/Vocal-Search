06c5310a06cf567d5107a93a22090b9ce92f8cab
Gary Kovar <gkovar@gmail.com>
2017-08-25 08:44:32 -0400

    Initial method stubs for admin search

--- includes/class-admin-search.php
+++ includes/class-admin-search.php
@@ -102,52 +102,42 @@ class VS_Admin_Search {
 			'option_key'   => self::$key, // The option key and admin menu page slug.
 			// 'icon_url'        => 'dashicons-palmtree', // Menu icon. Only applicable if 'parent_slug' is left empty.
 			// 'menu_title'      => esc_html__( 'Options', 'cmb2' ), // Falls back to 'title' (above).
-			// 'parent_slug'     => 'themes.php', // Make options page a submenu item of the themes menu.
+			'parent_slug'        => 'null', // Make options page a submenu item of the themes menu.
 			// 'capability'      => 'manage_options', // Cap required to view options-page.
 			// 'position'        => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
 			// 'admin_menu_hook' => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
-			// 'display_cb'      => false, // Override the options-page form output (CMB2_Hookup::options_page_output()).
+			'display_cb'         => array( $this, 'search_results' ), // Override the options-page form output (CMB2_Hookup::options_page_output()).
 			// 'save_button'     => esc_html__( 'Save Theme Options', 'cmb2' ), // The text for the options-page save button. Defaults to 'Save'.
-		) );
-
-		// Add your fields here.
-		$cmb->add_field( array(
-			'name'    => __( 'Test Text', 'vocal-search' ),
-			'desc'    => __( 'field description (optional)', 'vocal-search' ),
-			'id'      => 'test_text', // No prefix needed.
-			'type'    => 'text',
-			'default' => __( 'Default Text', 'vocal-search' ),
 		) );
 
 	}
 
-	/**
-	 * Wrapper function around cmb2_get_option.
-	 *
-	 * @since  0.1.0
-	 *
-	 * @param  string $key     Options array key
-	 * @param  mixed  $default Optional default value
-	 * @return mixed           Option value
-	 */
-	public static function get_value( $key = '', $default = false ) {
-		if ( function_exists( 'cmb2_get_option' ) ) {
+	public function search_results() {
+		echo '<h2>Search Results</h2>';
 
-			// Use cmb2_get_option as it passes through some key filters.
-			return cmb2_get_option( self::$key, $key, $default );
-		}
+		$posts = $this->get_posts();
+	}
 
-		// Fallback to get_option if CMB2 is not loaded yet.
-		$opts = get_option( self::$key, $default );
+	private function get_posts() {
+		$args = array(
+			'post_type' => $this->get_post_types(),
+			's' => $_GET['s'],
+			'post_status' => $this->get_post_stati(),
+		);
 
-		$val = $default;
+		$posts = new WP_Query( $args);
+		return $posts;
+	}
 
-		if ( 'all' == $key ) {
-			$val = $opts;
-		} elseif ( is_array( $opts ) && array_key_exists( $key, $opts ) && false !== $opts[ $key ] ) {
-			$val = $opts[ $key ];
-		}
+	private function get_post_types() {
+		$post_types = get_post_types( array(), 'names' );
 
-		return $val;
+		return array_keys( $post_types );
+	}
+
+	private function get_post_stati() {
+		$post_stati = get_post_stati();
+
+		return array_keys( $post_stati );
 	}
 }
